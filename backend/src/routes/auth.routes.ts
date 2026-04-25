0ce5d6d81cd38c43c27525311c8623dc873219d0
AlejandroMaldonadoMoreno <124050449@upq.edu.mx>
2026-04-24 18:33:19 -0600

    Resuelve conflicto y conserva la base de datos local

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
-import { register } from '../controllers/auth.controller';
+import { register, login, logout } from '../controllers/auth.controller';
+import { verifyJWT } from '../middlewares/auth.middleware';
 
 const router = Router();
 
 router.post('/register', register);
+
+router.post('/login', login);
+
+router.post('/logout', verifyJWT, logout);
 
 export default router;
